7b38ba17f17287cb9e7aac777ea43bfdd4db4ba6
Kirthik Raja Kesavan <s4570170@nodelogin04.alice.leidenuniv.nl>
2026-08-29 17:09:59 +0200

    Fix torch version to 2.5.1 and increase download time limit to 6h

--- jobs/job_download.sh
+++ jobs/job_download.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --time=01:00:00
+#SBATCH --time=06:00:00
 #SBATCH --partition=cpu-short
 
 echo "========================================"
