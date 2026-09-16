b1e3199f29ecefb9710767c7fc967057fe384711
Kirthik Raja <kirthikraja200@gmail.com>
2026-09-16 11:17:27 +0200

    Switch flat training to gpu-mig-40g; revert batch size to 4

--- jobs/job_train_flat.sh
+++ jobs/job_train_flat.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=40G
 #SBATCH --time=04:00:00
-#SBATCH --partition=gpu-short
+#SBATCH --partition=gpu-mig-40g
 #SBATCH --gres=gpu:1
 
 echo "========================================"
