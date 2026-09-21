41ead9ec473b23cff67f62f91fd49e7ac848465a
Kirthik Raja <kirthikraja200@gmail.com>
2026-09-21 14:49:52 +0200

    Switch chunked and balanced training to gpu-mig-40g with 12h limit

--- jobs/job_train_chunked.sh
+++ jobs/job_train_chunked.sh
@@ -6,8 +6,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=40G
-#SBATCH --time=04:00:00
-#SBATCH --partition=gpu-short
+#SBATCH --time=12:00:00
+#SBATCH --partition=gpu-mig-40g
 #SBATCH --gres=gpu:1
 
 echo "========================================"
